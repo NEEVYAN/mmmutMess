315c6688430b6cab5e1408f205f66b25fe69aa8d
AnnapuranaArmyFirst <neerajcoc1455@gmail.com>
2024-05-15 15:35:28 +0530

    fourth

--- fireConfig.js
+++ fireConfig.js
@@ -1,11 +1,21 @@
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDWz7-CZZUot40PjN-1aSgAoHI6bqZ-2Dg",
+    authDomain: "mmmutmess.firebaseapp.com",
+    projectId: "mmmutmess",
+    storageBucket: "mmmutmess.appspot.com",
+    messagingSenderId: "245689652045",
+    appId: "1:245689652045:web:b5cb7f493d4c8146001d04",
+    measurementId: "G-T3VLQEECTG"
+  };
 
 firebase.initializeApp(firebaseConfig);
